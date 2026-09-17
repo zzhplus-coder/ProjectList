229ae571fc7330e12b6daf1bbbaa94e15d425c32
ZZHPLUS <794289783@proton.me>
2026-09-17 21:43:57 +0800

    新增了一个练习模块

--- Project1/ConsoleApp-Demo1/Program.cs
+++ Project1/ConsoleApp-Demo1/Program.cs
@@ -1,22 +1,18 @@
-﻿//int fahrenheit = 94; //华氏度
+﻿#if false
+#region 华氏度转摄氏度练习
+int fahrenheit = 94; //华氏度
 
 //摄氏度 = (华氏度 - 32) x 5 / 9
 //5和9默认是整数int，需要转换成小数才可以让计算结果是小数
-//decimal celsius = (fahrenheit - 32) * 5m / 9m;
+decimal celsius = (fahrenheit - 32) * 5m / 9m;
 
-//Console.WriteLine($"The temperature is {celsius.ToString("F1")} Celsius.");
+Console.WriteLine($"The temperature is {celsius.ToString("F1")} Celsius.");
 
-//Console.WriteLine("Windows " + 1 + 1);
-//Console.WriteLine(5m / 10m);
+Console.WriteLine("Windows " + 1 + 1);
+Console.WriteLine(5m / 10m);
+#endregion
 
-
-////Sophia: 93, 87, 98, 95, 100
-
-//Nicolas: 80, 83, 82, 88, 85
-
-//Zahirah: 84, 96, 73, 85, 79
-
-//Jeong: 90, 92, 98, 100, 97
+#region 打印成绩练习
 
 //初始化成绩列表
 List<int> num0 = new List<int>
@@ -61,4 +57,63 @@ foreach (var item in nameList)
         rating = 'B';
     }
     Console.WriteLine(item.Key + "\t\t" + avgScore + " " + rating);
+}
+#endregion
+#endif
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("输入学生名字：");
+        string? input0 = Console.ReadLine();
+        string name = string.IsNullOrWhiteSpace(input0) ? "无名氏" : input0;
+
+        Console.Write("输入课程数量：");
+        int courseNum = Convert.ToInt32(Console.ReadLine());
+
+        List<Course> list = new List<Course>();
+
+        for (int i = 1; i <= courseNum; i++)
+        {
+            Console.Write($"输入第{i}个课程的名称：");
+            string? input1 = Console.ReadLine();
+            Console.Write($"输入第{i}个课程的成绩：");
+            double grade = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"输入第{i}个课程的信用小时数：");
+            double hours = Convert.ToDouble(Console.ReadLine());
+
+            Course course = new Course
+            {
+                Name = string.IsNullOrWhiteSpace(input1) ? "无科目名" : input1,
+                Grade = grade,
+                ReditHours = hours
+            };
+            list.Add(course);
+        }
+
+        Console.WriteLine($"学生：{name}");
+        Console.WriteLine($"\n课程\t成绩\t学分");
+
+        double totalGrade = 0;
+        double totalHours = 0;
+        foreach (Course course in list)
+        {
+            Console.WriteLine($"{course.Name}\t{course.Grade}\t{course.ReditHours}");
+            totalGrade += course.Grade * course.ReditHours;
+            totalHours += course.ReditHours;
+        }
+        double GPA = totalGrade / totalHours;
+        Console.WriteLine($"最终GPA：\t{GPA.ToString("F2")}");
+    }
+}
+
+/// <summary>
+/// 课程属性
+/// </summary>
+public class Course
+{
+    public string? Name { get; set; }
+    public double Grade { get; set; }
+    public double ReditHours { get; set; }
 }
